06cd1be4a1595084fc0b0e38bfed84272d4161e3
tsuyuni <tsuyu.lit@gmail.com>
2024-09-04 00:22:42 +0900

    Initial commit

---
+++ test/cdk_getting_started.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkGettingStarted from '../lib/cdk_getting_started-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_getting_started-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkGettingStarted.CdkGettingStartedStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
